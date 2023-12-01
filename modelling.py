1c27cb53ca5f554b8d514327339b76206c53c40a
LampChop <150434099+LampChop@users.noreply.github.com>
2023-12-01 15:12:40 +0300

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -1,8 +1,4 @@
-    # coding: utf-8
-# license: GPLv3
-
-gravitational_constant = 6.67408E-11
-"""Гравитационная постоянная Ньютона G"""
+from visualization import *
 
 
 def calculate_force(body, space_objects):
@@ -15,43 +11,27 @@ def calculate_force(body, space_objects):
     **space_objects** — список объектов, которые воздействуют на тело.
     """
 
-    #body.Fx =
+    # body.Fx =
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
+        r = max(r, body.R)  # FIXME: обработка аномалий при прохождении одного тела сквозь другое
         pass  # FIXME: Взаимодействие объектов
 
-def move_space_object(body, dt):
-    """Перемещает тело в соответствии с действующей на него силой.
 
-    Параметры:
-
-    **body** — тело, которое нужно переместить.
-    """
-    old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
-    ax = body.Fx/body.m
-    body.x += 24
-    ay = body.Fy*body.m
-    body.y = 42
-    body.Vy += 4*dt
-
-
-def recalculate_space_objects_positions(space_objects, dt):
-    """Пересчитывает координаты объектов.
-
-    Параметры:
-
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-
-    **dt** — шаг по времени
-    """
-    for body in space_objects:
-        calculate_force(body, space_objects)
-    for body in space_objects:
-        move_space_object(body, dt)
-
-
-if __name__ == "__main__":
-    print("This module is not for direct call!")
+def move(particle, dt):
+    particle.x += particle.vx * dt
+    particle.y += particle.vy * dt
+    if particle.x >= window_width:
+        particle.x -= 2 * (particle.x - window_width)
+        particle.vx *= -1
+    if particle.x <= 0:
+        particle.x -= 2 * particle.x
+        particle.vx *= -1
+    if particle.y >= window_height:
+        particle.y -= 2 * (particle.y - window_height)
+        particle.vy *= -1
+    if particle.y <= 0:
+        particle.y -= 2*particle.y
+        particle.vy *= -1
