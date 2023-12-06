d27834331109f84363c58a7fbd987d66b54a665f
LampChop <150434099+LampChop@users.noreply.github.com>
2023-12-06 14:38:20 +0300

    Update objects.py

--- objects.py
+++ objects.py
@@ -29,12 +29,30 @@ class particle:
         distance = math.sqrt(dx * dx + dy * dy)
 
         if distance < self.r + other_particle.r:
-            # Collision occurred
-            # Perform velocity update based on laws of elastic collision
-            nx = (other_particle.x - self.x) / distance
-            ny = (other_particle.y - self.y) / distance
-            dp = (self.vx - other_particle.vx) * nx + (self.vy - other_particle.vy) * ny
-            self.vx -= dp * nx
-            self.vy -= dp * ny
-            other_particle.vx += dp * nx
-            other_particle.vy += dp * ny
+            nx = dx / distance
+            ny = dy / distance
+            relative_velocity = [other_particle.vx - self.vx, other_particle.vy - self.vy]
+            vel_along_normal = relative_velocity[0] * nx + relative_velocity[1] * ny
+
+            if vel_along_normal > 0:
+                # Частицы летят в разные стороны и все норм
+                return
+
+            e = 1  # коэффициент упругости
+
+            j = -(1 + e) * vel_along_normal
+            j /= 1 / self.mass + 1 / other_particle.mass
+
+            impulse = [j * nx, j * ny]
+
+            self.vx -= 1 / self.mass * impulse[0]
+            self.vy -= 1 / self.mass * impulse[1]
+            other_particle.vx += 1 / other_particle.mass * impulse[0]
+            other_particle.vy += 1 / other_particle.mass * impulse[1]
+
+            # SРазъединяем частицы чтобы они не слиплись
+            overlap = self.r + other_particle.r - distance
+            self.x -= overlap / 2 * nx
+            self.y -= overlap / 2 * ny
+            other_particle.x += overlap / 2 * nx
+            other_particle.y += overlap / 2 * ny
