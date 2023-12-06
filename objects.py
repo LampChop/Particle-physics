4254a66cd7a60d7ac61c851ac082856ea23ea192
LampChop <150434099+LampChop@users.noreply.github.com>
2023-12-06 13:44:59 +0300

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 class particle:
     x = 0
     y = 0
@@ -21,3 +22,19 @@ class particle:
             (self.x, self.y),
             self.r
         )
+
+    def check_collision(self, other_particle):
+        dx = other_particle.x - self.x
+        dy = other_particle.y - self.y
+        distance = math.sqrt(dx * dx + dy * dy)
+
+        if distance < self.r + other_particle.r:
+            # Collision occurred
+            # Perform velocity update based on laws of elastic collision
+            nx = (other_particle.x - self.x) / distance
+            ny = (other_particle.y - self.y) / distance
+            dp = (self.vx - other_particle.vx) * nx + (self.vy - other_particle.vy) * ny
+            self.vx -= dp * nx
+            self.vy -= dp * ny
+            other_particle.vx += dp * nx
+            other_particle.vy += dp * ny
