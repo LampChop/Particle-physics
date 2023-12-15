7fc44657dd56a08cb66f1993ed6f5bf3035323d0
LampChop <150434099+LampChop@users.noreply.github.com>
2023-12-15 08:41:14 +0300

    useless stuff gone.py

--- visualization.py
+++ visualization.py
@@ -1,37 +1,4 @@
-# coding: utf-8
-# license: GPLv3
-
-import pygame as pg
-
-"""Модуль визуализации.
-Нигде, кроме этого модуля, не используются экранные координаты объектов.
-Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
-"""
-window_width = 800
-
+window_width = 1100
+ui_width = 300
 window_height = 600
-
 scale_factor = 100
-"""Масштабирование экранных координат по отношению к физическим.
-
-Тип: float
-
-Мера: количество пикселей на один метр."""
-
-
-
-class Drawer:
-    def __init__(self, screen):
-        self.screen = screen
-
-
-    def update(self, figures, ui):
-        self.screen.fill((0, 0, 0))
-        for figure in figures:
-            figure.draw(self.screen)
-        ui.blit()
-        ui.update()
-        pg.display.update()
-
-
-
